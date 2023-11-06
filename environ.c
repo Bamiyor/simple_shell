3e9055926a7460506e77fe2a198f849c5c39e907
mabelorji41 <mabelorji74@gmail.com>
2023-11-06 02:25:50 -0800

    C-simple shell update

---
+++ environ.c
@@ -0,0 +1,92 @@
+#include "shell.h"
+
+/*
+ * print_environment - prints the current environment
+ * @env: The environment variables
+ */
+void print_environment(char **env)
+{
+	for (int i = 0; env[i] != NULL; i++)
+	{
+		printf("%s\n", env[i]);
+	}
+}
+
+/**
+ * find_environment_variable - Find the value of an environment variable
+ * @env: The environment variables
+ * @name: The name of the environment variable to find
+ * Return: The value of the environment variable or NULL if not found
+ */
+
+char find_environment_variable(char *env, const char *name)
+{
+	for (int i = 0; env[i] != NULL; i++)
+	{
+		if (strstr(env[i], name) == env[i])
+		{
+			char *value = strchr(env[i], '=') + 1;
+
+			return (value);
+			}
+	}
+	return (NULL);
+}
+
+/**
+ * set_environment_variable - Set or modify an environment variable
+ * @env: The environment variables
+ * @name: The name of the environment variable
+ * @value: the value to set
+ * Return: 0 if successful, -1 if an error occurs
+ */
+
+int set_environment_variable(char **env, const char *name, const char *value)
+{
+	char *variable = find_environment_variable(env, name);
+
+	if (variable)
+	{
+		snprintf(variable, strlen(name) + strlen(value) + 2, "%s=%s", name, value);
+
+			return (0);
+	}
+	return (-1);
+}
+
+/**
+ * unset_environment_variable - Remove an environment variable
+ * @env: The environment variables
+ * @name: The name of the environment variable to remove
+ * Return: 0 if successful, -1 if the variable is not found
+ */
+
+int unset_environment_variable(char **env, const char *name)
+{
+	for (int i = 0; env[i] != NULL; i++)
+	{
+		if (strstr(env[i], name) == env[i])
+		{
+			for (int j = i; env[j] != NULL; j++)
+			{
+				env[j] = env[j + 1];
+			}
+			return (0);
+		}
+	}
+	return (-1);
+}
+
+/**
+ * populate_environment - Populates the environment variables
+ * @env: The environment variables
+ * @environ: The original environment variables
+ */
+void populate_environment(char *env, char *environ)
+{
+	for (int i = 0; environ[i] != NULL; i++)
+{
+	env[i] = strdup(environ[i]);
+}
+env[i] = NULL;
+}
---
+++ exit.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+
+/**
+ * _strncpy - Copy a string with a specified length
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of characters to copy
+ * Return: A pointer to the destinatio string
+ */
+
+char *_strncpy(char *dest, const char *src, int n)
+{
+	char *start = dest;
+
+	while (n > 1 && *src)
+	{
+		*dest++ = *src++;
+
+		n--;
+	}
+
+	while (n > 1)
+	{
+		*dest++ = '\0';
+		n--;
+	}
+
+	*dest = '\0';
+
+	return (start);
+}
+
+/**
+ * _strncat - Concatenate two strings with a specified length
+ * @dest; The destination string
+ * @src: The source string
+ * @n: The maximum number of chracters to append from src
+ *
+ * Return: A pointer to the destintion string
+ */
+
+char *_strncat(char *dest, const char *src, int n)
+{
+	char *start = dest;
+
+	while (*dest)
+	{
+		dest++;
+	}
+
+	while (n > 0 && *src)
+	{
+		*dest++ = *src++;
+
+		n--;
+	}
+
+	*dest = '\0';
+
+	return (start);
+}
+
+/**
+ * _strchr - Locate a character in a string
+ * @s: The string to search
+ * @c: The character to find
+ *
+ * Return: A pointer to the first occurence of the character or NULL if  not found
+ */
+
+char *_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (char *)s;
+		}
+		s++;
+	}
+
+	return (NULL);
+}
