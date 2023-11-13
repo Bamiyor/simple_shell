365c24fc5b5f52a5b90e8c4f3d7286e10b1458e0
Bamiyor <Bamiyor>
2023-11-13 12:56:21 -0800

    simpleshell

---
+++ string_ops.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+* string_length- obtains the length of a string
+* @s: the string whose length to check
+*
+* Return: integer length of string
+*/
+int string_length(char *s)
+{
+int length = 0;
+
+if (!s)
+return (0);
+
+while (*s++)
+length++;
+return (length);
+}
+
+/**
+* compare_strings - performs lexicogarphic comparison of two strangs.
+* @s1: the first string
+* @s2: the second string
+*
+* Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+*/
+int compare_strings(char *s1, char *s2)
+{
+while (*s1 && *s2)
+{
+if (*s1 != *s2)
+return (*s1 - *s2);
+s1++;
+s2++;
+}
+if (*s1 == *s2)
+return (0);
+else
+return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+* starts_with - checks if needle starts with haystack
+* @haystack: string to search
+* @needle: the substring to find
+*
+* Return: The address of next char of haystack or NULL
+*/
+char *starts_with(const char *haystack, const char *needle)
+{
+while (*needle)
+if (*needle++ != *haystack++)
+return (NULL);
+return ((char *)haystack);
+}
+
+/**
+* Concatenates two strings
+* @dest: the destination buffer
+* @src: the source buffer
+*
+* Return: pointer to destination buffer
+*/
+char *string_concatenate(char *dest, char *src)
+{
+char *result = dest;
+
+while (*dest)
+dest++;
+while (*src)
+*dest++ = *src++;
+*dest = *src;
+return (result);
+}
