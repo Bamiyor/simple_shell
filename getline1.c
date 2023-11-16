46caac9ef6bfec631a8db413fff0ceebec0ba7a5
Bamiyor <Bamiyor>
2023-11-16 01:33:06 -0800

    shell

--- getline1.c
+++ getline1.c
@@ -1,74 +1,69 @@
 #include "shell.h"
 
 /**
- * input_buffer - buffers chained commands
- * @info: Parameter struct
- * @buf: The address of buffer
- * @len: Address of len variable
+ * input_buf - buffers chained commands
+ * @info: parameter struct
+ * @buf: address of buffer
+ * @len: address of len var
  *
- * Return: Number of bytes read
+ * Return: bytes read
  */
-
-ssize_t input_buffer(info_t info, char *buf, size_t *len)
+ssize_t input_buf(info_t *info, char **buf, size_t *len)
 {
 ssize_t r = 0;
 size_t len_p = 0;
 
-if (!len)
+if (!*len)
 {
-free(*char);
-*buf == NULL;
+free(*buf);
+*buf = NULL;
 signal(SIGINT, sigintHandler);
-
 #if USE_GETLINE
 r = getline(buf, &len_p, stdin);
 #else
-r = custom_getline(info, buf, &len_p);
+r = _getline(info, buf, &len_p);
 #endif
-
 if (r > 0)
 {
-if ((*buf)(r - 1) == '\n')
+if ((*buf)[r - 1] == '\n')
 {
-(buf)[r - 1] = '\0';
+(*buf)[r - 1] = '\0';
 r--;
 }
+info->linecount_flag = 1;
+remove_comments(*buf);
 
-info_t->linecount_flag = 1;
-remove_comments(*char);
-build_historyList(info, *buf, info_t->histcount++);
-
+{
 *len = r;
-info_t->cmd_buf = buf;
+info->cmd_buf = buf;
 }
 }
-return (bytes_read);
+}
+return (r);
 }
 
 /**
- * get_input - gets a line minus the newline
- * @info: Parameter struct
+ * get_input - It gets a line minus the newline
+ * @info: parameter struct
  *
- * Return: Bytes read
+ * Return: bytes read
  */
-
 ssize_t get_input(info_t *info)
 {
-static char buf;
+static char *buf;
 static size_t i, j, len;
-ssize_t bytes_read = 0;
+ssize_t r = 0;
 char **buf_p = &(info->arg), *p;
 
 _putchar(BUF_FLUSH);
-bytes_read = input_buffer(info_t, &buf, &len);
-
-if (bytes_read == -1)
+r = input_buf(info, &buf, &len);
+if (r == -1)
 return (-1);
-
 if (len)
 {
 j = i;
 p = buf + i;
+
 check_chain(info, buf, &j, i, len);
 while (j < len)
 {
@@ -79,89 +74,79 @@ j++;
 i = j + 1;
 if (i >= len)
 {
-
-
-/*
-* Reset position and length
-*/
-
 i = len = 0;
 info->cmd_buf_type = CMD_NORM;
 }
+
 *buf_p = p;
 return (_strlen(p));
 }
 
-/*
-* Not a chain; pass back buffer from custom_getline()
-*/
-
 *buf_p = buf;
-return (bytes_read);
+return (r);
 }
 
 /**
- * read_buffer - reads a buffer
- * @info: Paramter struct
- * @buf: Buffer
- * @i: Size
+ * read_buf - reads a buffer
+ * @info: parameter struct
+ * @buf: buffer
+ * @i: size
  *
- * Return: bytes_read
+ * Return: r
  */
-
-ssize_t read_buffer(info_t *info, char *buf, size_t *i)
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
 {
-ssize_t bytes_read = 0;
+ssize_t r = 0;
+
 if (*i)
 return (0);
-bytes_read = read(info->readfd, buf, READ_BUF_SIZE);
-if (bytes_read >= 0)
-*i = bytes_read;
-return (bytes_read);
+r = read(info->readfd, buf, READ_BUF_SIZE);
+if (r >= 0)
+*i = r;
+return (r);
 }
 
 /**
- * custom_getline - gets the next line of input from STDIN
- * @info: Parameter struct
- * @ptr: Address of pointer to buffer, preallocated or NULL
+ * _getline - gets the next line of input from STDIN
+ * @info: parameter struct
+ * @ptr: address of pointer to buffer, preallocated or NULL
  * @length: size of preallocated ptr buffer if not NULL
  *
  * Return: s
  */
-
-int custom_getline(info_t info, char *ptr, size_t *length)
+int _getline(info_t *info, char **ptr, size_t *length)
 {
 static char buf[READ_BUF_SIZE];
 static size_t i, len;
 size_t k;
-ssize_t bytes_read = 0, s = 0;
+ssize_t r = 0, s = 0;
 char *p = NULL, *new_p = NULL, *c;
+
 p = *ptr;
 if (p && length)
 s = *length;
 if (i == len)
 i = len = 0;
-	bytes_read = read_buffer(info, buf, &len);
-if (bytes_read == -1 || (bytes_read == 0 && len == 0))
+
+r = read_buf(info, buf, &len);
+if (r == -1 || (r == 0 && len == 0))
 return (-1);
+
 c = _strchr(buf + i, '\n');
 k = c ? 1 + (unsigned int)(c - buf) : len;
 new_p = _realloc(p, s, s ? s + k : k + 1);
 if (!new_p)
-{
-return (p ? (free(p), -1) : -1);
-}
+return (p ? free(p), -1 : -1);
+
 if (s)
-{
 _strncat(new_p, buf + i, k - i);
-}
 else
-{
 _strncpy(new_p, buf + i, k - i + 1);
-}
+
 s += k - i;
 i = k;
 p = new_p;
+
 if (length)
 *length = s;
 *ptr = p;
@@ -170,10 +155,13 @@ return (s);
 
 /**
  * sigintHandler - blocks ctrl-C
- * @sig_num: The signal number
+ * @sig_num: the signal number
+ *
+ * Return: void
  */
-void sigintHandler(_attribute_((unused))int sig_num)
+void sigintHandler(__attribute__((unused))int sig_num)
 {
-_puts("\n$");
+_puts("\n");
+_puts("$ ");
 _putchar(BUF_FLUSH);
 }
--- shell.h
+++ shell.h
@@ -316,10 +316,9 @@ char *_strchr(const char *s, int c);
  * getline1.c
  */
 
-ssize_t input_buffer(info_t info, char *buf, size_t *len);
+ssize_t input_buf(info_t *info, char **buf, size_t *len);
 ssize_t get_input(info_t *info);
-ssize_t read_buffer(info_t *info, char *buf, size_t *i);
-int custom_getline(info_t info, char *ptr, size_t *length);
+ssize_t read_buf(info_t *info, char *buf, size_t *i);
 void sigintHandler(__attribute__((unused)) int sig_num);
 
 
